54aa192103d91b91f1d9b0d5fea277562ed74060
Ege <egecam000@gmail.com>
2023-06-10 00:43:03 +0300

    edited urls

--- backend/Cocktails/urls.py
+++ backend/Cocktails/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index),
+    path('home', views.home),
+    path('login', views.login),
+    path('register', views.register),
 ]
