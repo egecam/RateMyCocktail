f837c3be396a9f751c94d5292ff2c292fec8f5ff
yigitozman <108322277+yigitozman@users.noreply.github.com>
2023-06-09 23:47:41 +0300

    fixing stuff

--- backend/Cocktails/models.py
+++ backend/Cocktails/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=25),
-    password = models.CharField(max_length=25),
+    username = models.CharField(max_length=25)
+    password = models.CharField(max_length=25)
     avatar = models.ImageField(
-        upload_to='avatars/', default='avatars/default.png'),
+        upload_to='avatars/', default='avatars/default.png')
 
 
 class Recipe(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE),
-    title = models.CharField(max_length=25),
-    body = models.CharField(max_length=1000),
-    photo = models.ImageField(upload_to='recipes/'),
-    rating = models.FloatField(),
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=25)
+    body = models.CharField(max_length=1000)
+    photo = models.ImageField(upload_to='recipes/')
+    rating = models.FloatField()
