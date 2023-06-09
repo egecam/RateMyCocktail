e833b827b7b48fad579941f9e88f1d88986a22b6
Ege <egecam000@gmail.com>
2023-06-10 00:44:43 +0300

    theCocktailDB API connection

--- backend/Cocktails/views.py
+++ backend/Cocktails/views.py
@@ -1,6 +1,25 @@
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
+
+rate = 3.5
 
 
 def index(req):
     return HttpResponse("Hello, world. You're at the cocktails index.")
+
+
+def login(req):
+    return HttpResponse("This is login page.")
+
+
+def register(req):
+    return HttpResponse("This is register page.")
+
+
+def home(request):
+    response = requests.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    cocktails = response.json()['drinks']
+    return render(request, "cocktails.html", {"cocktails": cocktails, "rate": rate})
+    pass
