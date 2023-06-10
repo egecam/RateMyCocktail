a66b0a716e151cef5a279d13db631b3fd153c11a
Ege <egecam000@gmail.com>
2023-06-11 00:07:52 +0300

    Update views.py

--- backend/Cocktails/views.py
+++ backend/Cocktails/views.py
@@ -43,3 +43,24 @@ def home(request):
     cocktails = response.json()['drinks']
     return render(request, "cocktails.html", {"cocktails": cocktails, "rate": rate})
     pass
+
+
+'''
+arrayOfIngredients.push(
+          drink.strIngredient1,
+          drink.strIngredient2,
+          drink.strIngredient3,
+          drink.strIngredient4,
+          drink.strIngredient5,
+          drink.strIngredient6,
+          drink.strIngredient7,
+          drink.strIngredient8,
+          drink.strIngredient9,
+          drink.strIngredient10,
+          drink.strIngredient11,
+          drink.strIngredient12,
+          drink.strIngredient13,
+          drink.strIngredient14,
+          drink.strIngredient15
+        );
+'''
