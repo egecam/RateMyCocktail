57a0ed71a3d5afe080414ca94cf11ab255ad1ae8
Ege <egecam000@gmail.com>
2023-06-10 23:55:01 +0300

    import User model

--- backend/Cocktails/models.py
+++ backend/Cocktails/models.py
@@ -1,9 +1,5 @@
 from django.db import models
-
-
-class User(models.Model):
-    username = models.CharField(max_length=25)
-    password = models.CharField(max_length=25)
+from django.contrib.auth.models import User
 
 
 class Recipe(models.Model):
