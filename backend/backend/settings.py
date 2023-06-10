11e8d71150aaf8897452a1b787ab830709300c76
Ege <egecam000@gmail.com>
2023-06-10 23:55:04 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOGIN_REDIRECT_URL = '/'
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
