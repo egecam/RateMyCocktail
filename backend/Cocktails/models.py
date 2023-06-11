4d85d475d14b3b331e207ac8a7180e0f56570ad9
Ege <egecam000@gmail.com>
2023-06-11 22:00:26 +0300

    delete photo attr from model

--- backend/Cocktails/models.py
+++ backend/Cocktails/models.py
@@ -6,5 +6,4 @@ class Recipe(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=25)
     body = models.CharField(max_length=1000)
-    photo = models.ImageField(upload_to='recipes/')
     rating = models.FloatField()
